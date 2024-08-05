94fa54235f0d65253e19879178f31047fc4eee3c
DESKTOP-ONCGL70\HP-1 <miraj.sagara@brahmweb.com>
2024-08-05 18:46:01 +0530

    Remove the step 1 to 5 and add new steps

--- src/Components/Navigation.js
+++ src/Components/Navigation.js
@@ -1,24 +1,28 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCheck } from '@fortawesome/free-solid-svg-icons';
 
 const Navigation = ({ currentStep, steps }) => {
-  const isStepComplete = (stepNumber) => {
-    return stepNumber < currentStep;
+  const stepPaths = {
+    'General Information': '/fillUpDetailForm',
+    'Functionality': '/page2',
+    'Technical Specifications': '/page3',
+    'Final Details': '/page4',
+    'Review Your Submission': '/reviewAndSubmit',
   };
 
   return (
-    <ul className="nav nav-pills">
+    <nav className="nav nav-pills flex-column flex-sm-row">
       {steps.map((step, index) => (
-        <li className="nav-item" key={index}>
-          <NavLink className={`nav-link ${currentStep === index + 1 ? 'active' : ''}`} to={`/${step.toLowerCase()}`}>
-            Step {index + 1} {isStepComplete(index + 1) && <FontAwesomeIcon icon={faCheck} style={{ color: 'green', marginLeft: '5px' }} />}
-          </NavLink>
-        </li>
+        <NavLink
+          key={index}
+          to={stepPaths[step]}
+          className={`flex-sm-fill text-sm-center nav-link ${currentStep === index + 1 ? 'active' : ''}`}
+        >
+          {step}
+        </NavLink>
       ))}
-    </ul>
+    </nav>
   );
 };
 
