07dfca1307826f4b57ac967e55d1ef40d9be2b1d
DESKTOP-ONCGL70\HP-1 <miraj.sagara@brahmweb.com>
2024-08-05 18:48:22 +0530

    Slove error

--- src/reducers/formFieldsSlice.js
+++ src/reducers/formFieldsSlice.js
@@ -1,34 +1,34 @@
 // src/features/formFields/formFieldsSlice.js
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 
-// Define an async thunk for fetching form fields
-export const fetchFormFields = createAsyncThunk(
-  'formFields/fetchFormFields',
+// Define an async thunk for fetching form groups
+export const fetchFormGroups = createAsyncThunk(
+  'formFields/fetchFormGroups',
   async () => {
-    const response = await fetch('http://localhost:5000/formFields');
+    const response = await fetch('http://localhost:5000/formGroups');
     const data = await response.json();
     return data;
   }
 );
 
 const formFieldsSlice = createSlice({
-  name: 'formFields',
+  name: 'formGroups',
   initialState: {
-    fields: [],
+    formGroups: [],
     status: 'idle',
     error: null
   },
   reducers: {},
   extraReducers: (builder) => {
     builder
-      .addCase(fetchFormFields.pending, (state) => {
+      .addCase(fetchFormGroups.pending, (state) => {
         state.status = 'loading';
       })
-      .addCase(fetchFormFields.fulfilled, (state, action) => {
+      .addCase(fetchFormGroups.fulfilled, (state, action) => {
         state.status = 'succeeded';
-        state.fields = action.payload;
+        state.formGroups = action.payload;
       })
-      .addCase(fetchFormFields.rejected, (state, action) => {
+      .addCase(fetchFormGroups.rejected, (state, action) => {
         state.status = 'failed';
         state.error = action.error.message;
       });
